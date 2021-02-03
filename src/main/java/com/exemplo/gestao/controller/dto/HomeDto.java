d21444a050819f68acd11d7b4fea7904e519d8f1
leo220 <leo.cavazzola@hotmail.com>
2021-02-03 19:39:06 -0300

    Adicionado HomeController e HomeDto

---
+++ src/main/java/com/exemplo/gestao/controller/dto/HomeDto.java
@@ -0,0 +1,16 @@
+package com.exemplo.gestao.controller.dto;
+
+import com.exemplo.gestao.controller.link.TaskLink;
+import lombok.Getter;
+import org.springframework.hateoas.RepresentationModel;
+
+@Getter
+public class HomeDto extends RepresentationModel<TaskDetailsDto> {
+
+    public HomeDto() {
+        super.add(
+                TaskLink.readAll(null, null),
+                TaskLink.create()
+        );
+    }
+}
