bd41f741bf9e5d6aa454d9fb967c067d8d2b5834
leoca <leo.cavazzola@hotmail.com>
2022-02-02 20:10:35 -0300

    update user

--- src/main/java/com/exemple/api/entity/User.java
+++ src/main/java/com/exemple/api/entity/User.java
@@ -2,9 +2,11 @@ package com.exemple.api.entity;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -13,52 +15,66 @@ import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @EqualsAndHashCode(of = {"email"})
 @Entity
 public class User implements UserDetails {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String id;
-	private String email;
-	private String password;
-	private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String email;
+    private String password;
+    private String name;
 
-	@ManyToMany(fetch = FetchType.EAGER)
-	private List<Role> roles = new ArrayList<>();
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles = new ArrayList<>();
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return this.roles;
-	}
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = new BCryptPasswordEncoder().encode(password);
+        this.name = name;
+    }
 
-	@Override
-	public String getPassword() {
-		return this.password;
-	}
+    public User update(User right) {
+        this.email = right.getEmail();
+        this.password = right.getPassword();
+        this.name = right.getName();
+        return this;
+    }
 
-	@Override
-	public String getUsername() {
-		return this.email;
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
