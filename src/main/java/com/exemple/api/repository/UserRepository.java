3c26f3a4fc98a20e7e6b2427d8fab74c124ad4ff
leoca <leo.cavazzola@hotmail.com>
2022-02-02 20:06:40 -0300

    update repositories

--- src/main/java/com/exemple/api/repository/UserRepository.java
+++ src/main/java/com/exemple/api/repository/UserRepository.java
@@ -2,11 +2,9 @@ package com.exemple.api.repository;
 
 import java.util.Optional;
 
-import com.exemple.api.model.Usuario;
+import com.exemple.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Usuario, Long> {
-
-	Optional<Usuario> findByEmail(String email);
-
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
 }
