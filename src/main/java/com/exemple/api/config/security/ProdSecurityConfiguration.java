f11d22bb346e4fc5ebe287b1eec024f5aabedb49
leoca <leo.cavazzola@hotmail.com>
2022-02-02 20:14:22 -0300

    update security configurations

---
+++ src/main/java/com/exemple/api/config/security/ProdSecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.exemple.api.config.security;
+
+import com.exemple.api.config.security.filter.JWTFilter;
+import com.exemple.api.common.helper.JWTHelper;
+import com.exemple.api.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+@Profile("prod")
+public class ProdSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final JWTHelper jwtHelper;
+    private final UserService userService;
+
+    public ProdSecurityConfiguration(JWTHelper jwtHelper, UserService userService) {
+        this.jwtHelper = jwtHelper;
+        this.userService = userService;
+    }
+
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //general:
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable();
+
+        //access management:
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated();
+
+        //filters
+        http.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    private JWTFilter jwtFilter() {
+        return new JWTFilter(jwtHelper, userService);
+    }
+}
