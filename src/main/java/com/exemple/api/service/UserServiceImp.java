05a1179f365803bfa776cbde18a5ad6ac99a90f8
leoca <leo.cavazzola@hotmail.com>
2022-02-02 20:10:20 -0300

    create user service

---
+++ src/main/java/com/exemple/api/service/UserServiceImp.java
@@ -0,0 +1,45 @@
+package com.exemple.api.service;
+
+import com.exemple.api.entity.User;
+import com.exemple.api.exception.EntityNotFoundException;
+import com.exemple.api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImp implements UserDetailsService, UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private User findEntity(Long id) {
+        return userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return this.findEntity(id);
+    }
+
+    @Override
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User update(Long id, User right) {
+        User updated = findEntity(id).update(right);
+        return userRepository.save(updated);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+    }
+}
