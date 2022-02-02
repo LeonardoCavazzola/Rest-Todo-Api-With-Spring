ed3cc5ae389b9b7e01fdf5342f1e2557ee48dcf3
leoca <leo.cavazzola@hotmail.com>
2022-02-02 20:11:41 -0300

    create jwt helper

---
+++ src/main/java/com/exemple/api/common/helper/JWTHelper.java
@@ -0,0 +1,49 @@
+package com.exemple.api.common.helper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTHelper {
+
+    @Value("${forum.jwt.duration}")
+    private String duration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String generate(String subject) {
+        Date today = new Date();
+        Date expiration = new Date(today.getTime() + Long.parseLong(duration));
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(today)
+                .setExpiration(expiration)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public Boolean isValid(String token) {
+        try {
+            Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Long getSubject(String token) {
+        String subject = Jwts.parser()
+                .setSigningKey(this.secret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        return Long.parseLong(subject);
+    }
+}
