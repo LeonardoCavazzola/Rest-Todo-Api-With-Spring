2006e19bb9fecce65c72b53574efcb2bb93ada8b
leoca <leo.cavazzola@hotmail.com>
2022-02-02 21:13:51 -0300

    add bean to create user when app run

--- src/main/java/com/exemple/api/Application.java
+++ src/main/java/com/exemple/api/Application.java
@@ -1,12 +1,22 @@
 package com.exemple.api;
 
+import com.exemple.api.domain.entity.User;
+import com.exemple.api.domain.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner createUser(UserService userService) {
+        return args -> userService.create(new User("email@email.com", "12345678", "user"));
+    }
 }
